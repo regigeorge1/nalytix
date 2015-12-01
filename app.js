d275e48bb5f4f52388b8ef373683c4e658b4c11f
regigeorge1 <regi.george@nalytix.com>
2015-11-30 21:14:48 -0600

    First nalytix business connect package
    The package with registration and login

---
+++ app.js
@@ -0,0 +1,276 @@
+var express = require('express'),
+    connect = require('connect'),
+    jade = require('jade'),
+    app = module.exports = express.createServer(),
+    mongoose = require('mongoose'),
+    mongoStore = require('connect-mongodb'),
+    mailer = require('mailer'),
+    stylus = require('stylus'),
+    markdown = require('markdown').markdown,
+    connectTimeout = require('connect-timeout'),
+    util = require('util'),
+    path = require('path'),
+    models = require('./models'),
+    db,
+    User,
+    LoginToken,
+    Settings = { development: {}, test: {}, production: {} },
+    emails;
+
+function renderJadeFile(template, options) {
+  var fn = jade.compile(template, options);
+  return fn(options.locals);
+}
+
+emails = {
+  send: function(template, mailOptions, templateOptions) {
+    mailOptions.to = mailOptions.to;
+    renderJadeFile(path.join(__dirname, 'views', 'mailer', template), templateOptions, function(err, text) {
+      // Add the rendered Jade template to the mailOptions
+      mailOptions.body = text;
+
+      // Merge the app's mail options
+      var keys = Object.keys(app.set('mailOptions')),
+          k;
+      for (var i = 0, len = keys.length; i < len; i++) {
+        k = keys[i];
+        if (!mailOptions.hasOwnProperty(k))
+          mailOptions[k] = app.set('mailOptions')[k]
+      }
+
+      console.log('[SENDING MAIL]', util.inspect(mailOptions));
+
+      // Only send mails in production
+      if (app.settings.env == 'production') {
+        mailer.send(mailOptions,
+          function(err, result) {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+      }
+    });
+  },
+
+  sendWelcome: function(user) {
+    this.send('welcome.jade', { to: user.email, subject: 'Welcome to the \'nalytix Business Connect' }, { locals: { user: user } });
+  }
+};
+
+app.helpers(require('./helpers.js').helpers);
+app.dynamicHelpers(require('./helpers.js').dynamicHelpers);
+
+app.configure('development', function() {
+  app.set('db-uri', 'mongodb://localhost/nalytix-development');
+  app.use(express.errorHandler({ dumpExceptions: true }));
+  app.set('view options', {
+    pretty: true
+  });
+});
+
+app.configure('test', function() {
+  app.set('db-uri', 'mongodb://localhost/nalytix-test');
+  app.set('view options', {
+    pretty: true
+  });  
+});
+
+app.configure('production', function() {
+  app.set('db-uri', 'mongodb://localhost/nalytix-production');
+});
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.use(express.favicon());
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(connectTimeout({ time: 10000 }));
+  app.use(express.session({ store: mongoStore(app.set('db-uri')), secret: 'topsecret' }));
+  app.use(express.logger({ format: '\x1b[1m:method\x1b[0m \x1b[33m:url\x1b[0m :response-time ms' }))
+  app.use(express.methodOverride());
+  app.use(stylus.middleware({ src: __dirname + '/public' }));
+  app.use(express.static(__dirname + '/public'));
+  app.set('mailOptions', {
+    host: 'localhost',
+    port: '25',
+    from: 'nodepad@example.com'
+  });
+});
+
+models.defineModels(mongoose, function() {
+  app.User = User = mongoose.model('User');
+  app.LoginToken = LoginToken = mongoose.model('LoginToken');
+  db = mongoose.connect(app.set('db-uri'));
+})
+
+function authenticateFromLoginToken(req, res, next) {
+  var cookie = JSON.parse(req.cookies.logintoken);
+
+  LoginToken.findOne({ email: cookie.email,
+                       series: cookie.series,
+                       token: cookie.token }, (function(err, token) {
+    if (!token) {
+      res.redirect('/sessions/new');
+      return;
+    }
+
+    User.findOne({ email: token.email }, function(err, user) {
+      if (user) {
+        req.session.user_id = user.id;
+        req.currentUser = user;
+
+        token.token = token.randomToken();
+        token.save(function() {
+          res.cookie('logintoken', token.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/' });
+          next();
+        });
+      } else {
+        res.redirect('/sessions/new');
+      }
+    });
+  }));
+}
+
+function loadUser(req, res, next) {
+  if (req.session.user_id) {
+    User.findById(req.session.user_id, function(err, user) {
+      if (user) {
+        req.currentUser = user;
+        next();
+      } else {
+        res.redirect('/sessions/new');
+      }
+    });
+  } else if (req.cookies.logintoken) {
+    authenticateFromLoginToken(req, res, next);
+  } else {
+    res.redirect('/sessions/new');
+  }
+}
+
+app.get('/', loadUser, function(req, res) {
+  res.redirect('/documents')
+});
+
+// Error handling
+function NotFound(msg) {
+  this.name = 'NotFound';
+  Error.call(this, msg);
+  Error.captureStackTrace(this, arguments.callee);
+}
+
+util.inherits(NotFound, Error);
+
+app.get('/404', function(req, res) {
+  throw new NotFound;
+});
+
+app.get('/500', function(req, res) {
+  throw new Error('An expected error');
+});
+
+app.get('/bad', function(req, res) {
+  unknownMethod();
+});
+
+app.error(function(err, req, res, next) {
+  if (err instanceof NotFound) {
+    res.render('404.jade', { status: 404 });
+  } else {
+    next(err);
+  }
+});
+
+if (app.settings.env == 'production') {
+  app.error(function(err, req, res) {
+    res.render('500.jade', {
+      status: 500,
+      locals: {
+        error: err
+      } 
+    });
+  });
+}
+
+
+// Users
+app.get('/users/new', function(req, res) {
+  res.render('users/new.jade', {
+    locals: { user: new User() }
+  });
+});
+
+app.post('/users.:format?', function(req, res) {
+  var user = new User(req.body.user);
+
+  function userSaveFailed() {
+    req.flash('error', 'Account creation failed');
+    res.render('users/new.jade', {
+      locals: { user: user }
+    });
+  }
+
+  user.save(function(err) {
+    if (err) return userSaveFailed();
+
+    req.flash('info', 'Your account has been created');
+    emails.sendWelcome(user);
+
+    switch (req.params.format) {
+      case 'json':
+        res.send(user.toObject());
+      break;
+
+      default:
+        req.session.user_id = user.id;
+        res.redirect('/documents');
+    }
+  });
+});
+
+// Sessions
+app.get('/sessions/new', function(req, res) {
+  res.render('sessions/new.jade', {
+    locals: { user: new User() }
+  });
+});
+
+app.post('/sessions', function(req, res) {
+  User.findOne({ email: req.body.user.email }, function(err, user) {
+    if (user && user.authenticate(req.body.user.password)) {
+      req.session.user_id = user.id;
+
+      // Remember me
+      if (req.body.remember_me) {
+        var loginToken = new LoginToken({ email: user.email });
+        loginToken.save(function() {
+          res.cookie('logintoken', loginToken.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/' });
+          res.redirect('/documents');
+        });
+      } else {
+        res.redirect('/documents');
+      }
+    } else {
+      req.flash('error', 'Incorrect credentials');
+      res.redirect('/sessions/new');
+    }
+  }); 
+});
+
+app.del('/sessions', loadUser, function(req, res) {
+  if (req.session) {
+    LoginToken.remove({ email: req.currentUser.email }, function() {});
+    res.clearCookie('logintoken');
+    req.session.destroy(function() {});
+  }
+  res.redirect('/sessions/new');
+});
+
+if (!module.parent) {
+  app.listen(3000, 'localhost', function() {
+    console.log('Express server listening on port %d, environment: %s', app.address().port, app.settings.env)
+  });
+  
+  console.log('Using connect %s, Express %s, Jade %s', connect.version, express.version, jade.version);
+}
